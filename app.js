f74964110cf9c7771d2aa9b18cbd64a0b9683c6f
Pontus <pontus00valdes@gmail.com>
2022-04-27 08:15:26 +0200

    try 42

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fetch = require('node-fetch');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const GeoIP = require("simple-geoip");
+var ip = require('ip');
 dotenv.config();
 const PORT = process.env.PORT || 8080;
 var cookieSession = require('cookie-session');
@@ -15,22 +16,23 @@ app.use(bodyParser.json());
 
 
 let middleware = async (req, res, next) => {
-  const response = await fetch('https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=' + process.env.GEO_IP_TOKEN + '&ipAddress=8.8.8.8');
-  const results = await response.json();
   
-  req.lat = results.location.lat;
-  req.lng = results.location.lng;
+    const response = await fetch('https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=' + process.env.GEO_IP_TOKEN);
+    const results = await response.json();
+    req.lat = results.location.lat;
+    req.lng = results.location.lng;
+    
+ 
   next();
 }
 
 // api call + "protected route"
 
-app.get(process.env.SECRET_ROUTE, middleware, async (req, res) => {
+app.get(process.env.SECRET_ROUTE, middleware,  async (req, res) => {
 
     try {    
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${req.lat}&lon=${req.lng}&appid=${process.env.API_TOKEN}`)
         const results = await response.json();
-        
         const reject = () => {
             res.setHeader('www-authenticate', 'Basic')
             res.sendStatus(401)
---
+++ index.js
@@ -0,0 +1,15 @@
+let x = document.getElementById("location");
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+  }
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
